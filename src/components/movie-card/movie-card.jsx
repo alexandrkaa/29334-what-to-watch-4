107a369b68934b7e72f81d0e30f36728d047340a
Alexander Komarov <komar81@gmail.com>
2020-06-22 13:37:36 +0300

    Обновил компоненты согласно мокам и заданию

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MovieCard = (props) => {
-  const {title, image, onMovieCardMouseEnter, onMovieTitleClick} = props;
+  const {movie, onMovieCardMouseEnter, onMovieTitleClick} = props;
+  const {title, image} = movie;
+
+  const _onMovieCardMouseEnter = onMovieCardMouseEnter.bind(null, movie);
+  const _onMovieTitleClick = onMovieTitleClick.bind(null, movie);
   return (
-    <article className="small-movie-card catalog__movies-card" onMouseEnter={onMovieCardMouseEnter}>
-      <div className="small-movie-card__image">
+    <article className="small-movie-card catalog__movies-card" onMouseEnter={_onMovieCardMouseEnter}>
+      <div className="small-movie-card__image" onClick={_onMovieTitleClick}>
         <img src={image} alt={title} width="280" height="175" />
       </div>
-      <h3 className="small-movie-card__title" onClick={onMovieTitleClick}>
+      <h3 className="small-movie-card__title" onClick={_onMovieTitleClick}>
         <a
           className="small-movie-card__link"
           href="movie-page.html"
@@ -19,8 +23,22 @@ const MovieCard = (props) => {
 };
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    movieBackground: PropTypes.string.isRequired,
+    movieGenre: PropTypes.string.isRequired,
+    movieDate: PropTypes.string.isRequired,
+    movieImage: PropTypes.string.isRequired,
+    movieRatingScore: PropTypes.string.isRequired,
+    movieRatingLevel: PropTypes.string.isRequired,
+    movieRatingCount: PropTypes.string.isRequired,
+    movieDirector: PropTypes.string.isRequired,
+    movieStarring: PropTypes.string.isRequired,
+    movieDescription: PropTypes.arrayOf(
+        PropTypes.string.isRequired
+    ),
+  }),
   onMovieCardMouseEnter: PropTypes.func.isRequired,
   onMovieTitleClick: PropTypes.func.isRequired
 };
--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const MovieDetails = (props) => {
+  const {movie} = props;
+  const {movieDescription, movieDirector, movieStarring, movieImage, title,
+    movieGenre, movieDate, movieBackground, movieRatingScore, movieRatingLevel, movieRatingCount} = movie;
   return (
     <section className="movie-card movie-card--full">
       <div className="movie-card__hero">
         <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={movieBackground} alt={title} />
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -29,10 +32,10 @@ const MovieDetails = (props) => {
 
         <div className="movie-card__wrap">
           <div className="movie-card__desc">
-            <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+            <h2 className="movie-card__title">{title}</h2>
             <p className="movie-card__meta">
-              <span className="movie-card__genre">Drama</span>
-              <span className="movie-card__year">2014</span>
+              <span className="movie-card__genre">{movieGenre}</span>
+              <span className="movie-card__year">{movieDate}</span>
             </p>
 
             <div className="movie-card__buttons">
@@ -57,7 +60,7 @@ const MovieDetails = (props) => {
       <div className="movie-card__wrap movie-card__translate-top">
         <div className="movie-card__info">
           <div className="movie-card__poster movie-card__poster--big">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            <img src={movieImage} alt={title} width="218" height="327" />
           </div>
 
           <div className="movie-card__desc">
@@ -76,27 +79,47 @@ const MovieDetails = (props) => {
             </nav>
 
             <div className="movie-rating">
-              <div className="movie-rating__score">8,9</div>
+              <div className="movie-rating__score">{movieRatingScore}</div>
               <p className="movie-rating__meta">
-                <span className="movie-rating__level">Very good</span>
-                <span className="movie-rating__count">240 ratings</span>
+                <span className="movie-rating__level">{movieRatingLevel}</span>
+                <span className="movie-rating__count">{movieRatingCount}</span>
               </p>
             </div>
 
             <div className="movie-card__text">
-              <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.</p>
-
-              <p>Gustave prides himself on providing first-className service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-
-              <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-              <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+              {
+                movieDescription.map((it, id) => {
+                  return (
+                    <p key={id}>{it}</p>
+                  );
+                })
+              }
+              <p className="movie-card__director"><strong>Director: {movieDirector}</strong></p>
+              <p className="movie-card__starring"><strong>Starring: {movieStarring}</strong></p>
             </div>
           </div>
         </div>
       </div>
     </section>
   );
+};
+
+MovieDetails.propTypes = {
+  movie: PropTypes.shape({
+    movieDescription: PropTypes.arrayOf(
+        PropTypes.string.isRequired
+    ),
+    movieDirector: PropTypes.string.isRequired,
+    movieStarring: PropTypes.string.isRequired,
+    movieImage: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    movieGenre: PropTypes.string.isRequired,
+    movieDate: PropTypes.string.isRequired,
+    movieBackground: PropTypes.string.isRequired,
+    movieRatingScore: PropTypes.string.isRequired,
+    movieRatingLevel: PropTypes.string.isRequired,
+    movieRatingCount: PropTypes.string.isRequired,
+  })
 };
 
 export default MovieDetails;

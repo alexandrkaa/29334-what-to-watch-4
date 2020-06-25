4d730e66ad2ddaa53041a39b1497fa42385ea5ff
Alexander Komarov <komar81@gmail.com>
2020-06-25 19:01:01 +0300

    Начал создание withVideoPlayer HOC

---
+++ src/hocs/with-video-player.js
@@ -0,0 +1,82 @@
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
+
+
+export const withVideoPlayer = (Component) => {
+  class VideoPlayerHoc extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this._videoRef = createRef();
+
+      this.state = {
+        progress: 0,
+        isLoading: true,
+        isPlaying: props.isPlaying,
+        isMuted: props.isMuted,
+      };
+    }
+
+    componentDidMount() {
+      const {src} = this.props;
+      const video = this._videoRef.current;
+      video.src = src;
+
+      video.oncanplay = () => {
+        this.setState({
+          isLoading: false
+        });
+      };
+
+      video.onplay = () => {
+        this.setState({
+          isPlaying: true,
+        });
+      };
+
+      video.onpause = () => {
+        this.setState({
+          isPlaying: false,
+        });
+      };
+
+      video.ontimeupdate = () => {
+        this.setState({
+          progress: video.currentTime,
+        });
+      };
+    }
+
+    componentWillUnmount() {
+      const video = this._videoRef.current;
+      video.oncanplay = null;
+      video.onplay = null;
+      video.onpause = null;
+      video.ontimeupdate = null;
+      video.src = ``;
+    }
+
+    render() {
+      return <Component ref={this._videoRef} {...this.props} />;
+    }
+
+    componentDidUpdate() {
+      const video = this._videoRef.current;
+
+      if (this.props.isPlaying) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }
+  }
+
+  VideoPlayerHoc.propTypes = {
+    src: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    isPlaying: PropTypes.bool.isRequired,
+    isMuted: PropTypes.bool.isRequired,
+  };
+};
+
+export default withVideoPlayer;

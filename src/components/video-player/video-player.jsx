545e14792efb02a34b31ab09a039678dc3762c80
Alexander Komarov <komar81@gmail.com>
2020-06-26 10:21:37 +0300

    VideoPlayer сделал функциональным компонентом

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,82 +1,20 @@
-import React, {PureComponent, createRef} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class VideoPlayer extends PureComponent {
-  constructor(props) {
-    super(props);
+const VideoPlayer = React.forwardRef((ref) => {
+  return (
+    <video ref={ref} controls={false} autoPlay={false} width="280" height="175" />
+  );
+});
 
-    this._videoRef = createRef();
-
-    this.state = {
-      progress: 0,
-      isLoading: true,
-      isPlaying: props.isPlaying,
-      isMuted: props.isMuted,
-    };
-  }
-
-  componentDidMount() {
-    const {src} = this.props;
-    const video = this._videoRef.current;
-    video.src = src;
-
-    video.oncanplay = () => {
-      this.setState({
-        isLoading: false
-      });
-    };
-
-    video.onplay = () => {
-      this.setState({
-        isPlaying: true,
-      });
-    };
-
-    video.onpause = () => {
-      this.setState({
-        isPlaying: false,
-      });
-    };
-
-    video.ontimeupdate = () => {
-      this.setState({
-        progress: video.currentTime,
-      });
-    };
-  }
-
-  componentWillUnmount() {
-    const video = this._videoRef.current;
-    video.oncanplay = null;
-    video.onplay = null;
-    video.onpause = null;
-    video.ontimeupdate = null;
-    video.src = ``;
-  }
-
-  render() {
-    const {poster, src} = this.props;
-    return (
-      <video ref={this._videoRef} src={src} poster={poster} controls={false} autoPlay={false} width="280" height="175" />
-    );
-  }
-
-  componentDidUpdate() {
-    const video = this._videoRef.current;
-
-    if (this.props.isPlaying) {
-      video.play();
-    } else {
-      video.pause();
-    }
-  }
-}
+VideoPlayer.displayName = `VideoPlayer`;
 
 VideoPlayer.propTypes = {
   src: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   isPlaying: PropTypes.bool.isRequired,
   isMuted: PropTypes.bool.isRequired,
+  videoRef: PropTypes.object,
 };
 
 export default VideoPlayer;

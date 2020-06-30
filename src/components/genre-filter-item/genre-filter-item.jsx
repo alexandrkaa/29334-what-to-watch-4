774f1d8dbaa8e665f02761844929035c2568b193
Alexander Komarov <komar81@gmail.com>
2020-06-30 13:28:34 +0300

    Перенес connect из <GenreFilterItem /> полностью в <GenreFilterList />

--- src/components/genre-filter-item/genre-filter-item.jsx
+++ src/components/genre-filter-item/genre-filter-item.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {isEqual} from '../../utils/filters.js';
-import {getMoviesByGenre} from '../../utils/filters.js';
-import {connect} from 'react-redux';
-import {ActionCreator} from '../../reducer/reducer.js';
-// import {movieGenres} from '../../mocks/film.js';
 
 const GenreFilterItem = (props) => {
-  const {movieGenre, activeGenre, onChangeActiveGenre, moviesList} = props;
-  const _onChangeActiveGenre = onChangeActiveGenre.bind(null, movieGenre, moviesList);
+  const {movieGenre, activeGenre, onChangeActiveGenre } = props;
+  const _onChangeActiveGenre = onChangeActiveGenre.bind(null, movieGenre);
   return (
     <li className={`catalog__genres-item ${isEqual(movieGenre, activeGenre) ? `catalog__genres-item--active` : ``}`}>
       <a href="#" onClick={_onChangeActiveGenre} className="catalog__genres-link">{movieGenre}</a>
@@ -20,39 +16,6 @@ GenreFilterItem.propTypes = {
   movieGenre: PropTypes.string.isRequired,
   activeGenre: PropTypes.string.isRequired,
   onChangeActiveGenre: PropTypes.func.isRequired,
-  moviesList: PropTypes.arrayOf(
-      PropTypes.exact({
-        movieDescription: PropTypes.arrayOf(
-            PropTypes.string.isRequired
-        ),
-        image: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
-        movieDirector: PropTypes.string.isRequired,
-        movieStarring: PropTypes.string.isRequired,
-        movieImage: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        movieGenre: PropTypes.string.isRequired,
-        movieDate: PropTypes.string.isRequired,
-        movieBackground: PropTypes.string.isRequired,
-        movieRatingScore: PropTypes.string.isRequired,
-        movieRatingLevel: PropTypes.string.isRequired,
-        movieRatingCount: PropTypes.string.isRequired,
-        movieRunTime: PropTypes.number,
-        moviePreview: PropTypes.string.isRequired,
-      })).isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  activeGenre: state.activeGenre,
-  moviesList: state.moviesList,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onChangeActiveGenre(movieGenre, movies) {
-    dispatch(ActionCreator.changeActiveGenre(movieGenre));
-    dispatch(ActionCreator.getMoviesDataByGenre(getMoviesByGenre(movies, movieGenre)));
-  },
-});
-
-export {GenreFilterItem};
-export default connect(mapStateToProps, mapDispatchToProps)(GenreFilterItem);
+export default GenreFilterItem;
--- src/components/genre-filter-list/genre-filter-list.jsx
+++ src/components/genre-filter-list/genre-filter-list.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import {removeSpaces} from '../../utils/common.js';
 import GenreFilterItem from '../genre-filter-item/genre-filter-item.jsx';
 import {connect} from 'react-redux';
+import {ActionCreator} from '../../reducer/reducer.js';
+import {getMoviesByGenre} from '../../utils/filters.js';
 
 const GenreFilterList = (props) => {
-  const {movieGenres} = props;
+  const {movieGenres, moviesList, onChangeActiveGenre, activeGenre} = props;
+  const _onChangeActiveGenre = (movieGenre) => {
+    onChangeActiveGenre(movieGenre, moviesList);
+  }
   return (
     <ul className="catalog__genres-list">
       {movieGenres.map((genre) => {
@@ -13,6 +18,8 @@ const GenreFilterList = (props) => {
           <GenreFilterItem
             key={removeSpaces(genre).toLowerCase()}
             movieGenre={genre}
+            activeGenre={activeGenre}
+            onChangeActiveGenre={_onChangeActiveGenre}
           />
         );
       })}
@@ -24,11 +31,42 @@ GenreFilterList.propTypes = {
   movieGenres: PropTypes.arrayOf(
       PropTypes.string.isRequired
   ).isRequired,
+  activeGenre: PropTypes.string.isRequired,
+  onChangeActiveGenre: PropTypes.func.isRequired,
+  moviesList: PropTypes.arrayOf(
+      PropTypes.exact({
+        movieDescription: PropTypes.arrayOf(
+            PropTypes.string.isRequired
+        ),
+        image: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        movieDirector: PropTypes.string.isRequired,
+        movieStarring: PropTypes.string.isRequired,
+        movieImage: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        movieGenre: PropTypes.string.isRequired,
+        movieDate: PropTypes.string.isRequired,
+        movieBackground: PropTypes.string.isRequired,
+        movieRatingScore: PropTypes.string.isRequired,
+        movieRatingLevel: PropTypes.string.isRequired,
+        movieRatingCount: PropTypes.string.isRequired,
+        movieRunTime: PropTypes.number,
+        moviePreview: PropTypes.string.isRequired,
+      })).isRequired,
 };
 
 const mapStateToProps = (state) => ({
+  activeGenre: state.activeGenre,
+  moviesList: state.moviesList,
   movieGenres: state.movieGenres
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  onChangeActiveGenre(movieGenre, movies) {
+    dispatch(ActionCreator.changeActiveGenre(movieGenre));
+    dispatch(ActionCreator.getMoviesDataByGenre(getMoviesByGenre(movies, movieGenre)));
+  },
+});
+
 export {GenreFilterList};
-export default connect(mapStateToProps)(GenreFilterList);
+export default connect(mapStateToProps, mapDispatchToProps)(GenreFilterList);

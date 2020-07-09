34d18a47070df30d80ba71a3ca0d34db687ed205
Alexander Komarov <komar81@gmail.com>
2020-07-09 18:50:08 +0300

    + внес правки по комментариям наставника

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -4,22 +4,18 @@ export const getFilteredMovies = (movies, movieGenre) => {
   return movies.filter((movie) => isSameText(movie.movieGenre, movieGenre));
 };
 
-// +
 export const getMoviesByGenre = (movies, movieGenre) => {
   return movieGenre === DEFAULT_GENRE ? movies : getFilteredMovies(movies, movieGenre);
 };
 
-// +
 export const getWithLimit = (movies, start, stop) => {
   return movies.slice(start, stop);
 };
 
-// +
 export const getGenresFromMovies = (movies) => {
   return Array.from(new Set(movies.map((movie) => movie.movieGenre)));
 };
 
-// +
 export const isSameText = (firstString, secondString) => {
   return firstString.toLowerCase() === secondString.toLowerCase();
 };
